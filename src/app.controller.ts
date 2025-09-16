5e05e5eaa47e69b5671cf8335cc961759631a4a5
jiaoTailang <shiyib3719@gmail.com>
2025-09-16 18:19:38 +0800

    feat(redis): 添加 Redis 模块并实现地理位置相关功能
    - 在 package.json 中添加 redis 依赖
    - 新增 RedisModule 和 RedisService
    - 实现地理位置添加（geoAdd）、查询（geoPos）和列表（geoList）功能
    - 在 AppController 中添加 addPos 路由，用于添加地理位置信息
    - 更新 AppModule，引入 RedisModule

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import { RedisService } from 'src/redis/redis.service';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Inject,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Inject(RedisService)
+  private redisService: RedisService;
+
+  @Get('addPos')
+  async addPos(
+    @Query('name') posName: string,
+    @Query('longitude') longitude: number,
+    @Query('latitude') latitude: number,
+  ) {
+    if (!posName || !longitude || !latitude) {
+      throw new BadRequestException('位置信息不全');
+    }
+    try {
+      await this.redisService.geoAdd('positions', posName, [
+        longitude,
+        latitude,
+      ]);
+    } catch (e) {
+      throw new BadRequestException(e);
+    }
+
+    return {
+      code: 200,
+      message: '添加成功',
+    };
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
