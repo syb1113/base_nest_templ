c4e13b1e3dbe44675fafd42475faf5ce9a9caaf0
jiaoTailang <shiyib3719@gmail.com>
2025-09-17 16:26:25 +0800

    feat aaa: 添加 aaa 模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AaaModule } from './aaa/aaa.module';
 
 @Module({
-  imports: [],
+  imports: [AaaModule],
   controllers: [AppController],
   providers: [AppService],
 })
