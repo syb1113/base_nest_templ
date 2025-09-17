e3e59d9896d6dc1b42d25bc521396543070a95c5
jiaoTailang <shiyib3719@gmail.com>
2025-09-17 16:17:18 +0800

    feat: 添加 Swagger 文档
    - 在 package.json 中添加 @nestjs/swagger 依赖
    - 在 app.controller.ts 中添加 Swagger 相关的装饰器，用于生成 API 文档
    - 在 main.ts 中集成 SwaggerModule，设置 API 文档的标题、描述等信息

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiParam, ApiQuery, ApiResponse } from '@nestjs/swagger';
+export class CccDto {
+  aaa: string;
+  bbb: number;
+  ccc: Array<string>;
+}
+export class CccVo {
+  aaa: number;
+  bbb: number;
+}
 
 @Controller()
 export class AppController {
@@ -8,5 +26,61 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @ApiOperation({ summary: '测试 aaa', description: 'aaa 描述' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'aaa 成功',
+    type: String,
+  })
+  @ApiQuery({
+    name: 'a1',
+    type: String,
+    description: 'a1 param',
+    required: false,
+    example: '1111',
+  })
+  @ApiQuery({
+    name: 'a2',
+    type: Number,
+    description: 'a2 param',
+    required: true,
+    example: 2222,
+  })
+  @Get('aaa')
+  aaa(@Query('a1') a1, @Query('a2') a2) {
+    console.log(a1, a2);
+    return 'aaa success';
+  }
+  @ApiOperation({ summary: '测试 bbb', description: 'bbb 描述' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'bbb 成功',
+    type: String,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'id 不合法',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'ID',
+    required: true,
+    example: 222,
+  })
+  @Get('bbb/:id')
+  bbb(@Param('id') id) {
+    console.log(id);
+    return 'bbb success';
+  }
+
+  @Post('ccc')
+  ccc(@Body('ccc') ccc: CccDto) {
+    console.log(ccc);
+
+    const vo = new CccVo();
+    vo.aaa = 111;
+    vo.bbb = 222;
+    return vo;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs-example')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('Nestjs-redis-demo')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
