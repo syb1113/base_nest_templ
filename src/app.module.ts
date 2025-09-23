a2b0d7ebc04ea4b5836758a8dbf6ab44e7c56c69
jiaoTailang <shiyib3719@gmail.com>
2025-09-23 17:00:30 +0800

    feat(schedule): 引入定时任务模块及示例代码 新增 @nestjs/schedule 和 cron 依赖，用于支持定时任务功能。

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module, OnApplicationBootstrap } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScheduleModule, SchedulerRegistry } from '@nestjs/schedule';
+import { TaskService } from './task.service';
+import { AaaModule } from './aaa/aaa.module';
 
 @Module({
-  imports: [],
+  imports: [ScheduleModule.forRoot(), AaaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TaskService],
 })
-export class AppModule {}
+export class AppModule implements OnApplicationBootstrap {
+  @Inject()
+  private schedulerRegistry: SchedulerRegistry;
+  onApplicationBootstrap() {
+    const crons = this.schedulerRegistry.getCronJobs();
+
+    crons.forEach((i, k) => {
+      i.stop();
+      this.schedulerRegistry.deleteCronJob(k);
+    });
+
+    const intervals = this.schedulerRegistry.getIntervals();
+    intervals.forEach((i) => {
+      const interval = this.schedulerRegistry.getInterval(i) as NodeJS.Timeout;
+      clearInterval(interval);
+      this.schedulerRegistry.deleteInterval(i);
+    });
+
+    const timeouts = this.schedulerRegistry.getTimeouts();
+    timeouts.forEach((i) => {
+      const timeout = this.schedulerRegistry.getTimeout(i) as NodeJS.Timeout;
+      clearTimeout(timeout);
+      this.schedulerRegistry.deleteTimeout(i);
+    });
+  }
+}
---
+++ src/task.service.ts
@@ -0,0 +1,25 @@
+import { AaaService } from './aaa/aaa.service';
+import { Inject, Injectable } from '@nestjs/common';
+import { Cron, CronExpression, Interval, Timeout } from '@nestjs/schedule';
+
+@Injectable()
+export class TaskService {
+  @Inject()
+  private aaaService: AaaService;
+  @Cron(CronExpression.EVERY_5_SECONDS, {
+    name: 'task1',
+  })
+  handleCron() {
+    console.log('task execute', this.aaaService.findAll());
+  }
+
+  @Interval('task2', 500)
+  task2() {
+    console.log('task2');
+  }
+
+  @Timeout('task3', 3000)
+  task3() {
+    console.log('task3');
+  }
+}
