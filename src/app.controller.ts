1e2f51a59493acd93064b99119f04c1650c6c8d3
jiaoTailang <shiyib3719@gmail.com>
2025-09-26 15:05:16 +0800

    feat(app): 添加 SSE 流式接口并启用 CORS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Sse } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Observable } from 'rxjs';
+import { exec } from 'child_process';
+import { readFileSync } from 'fs';
 
 @Controller()
 export class AppController {
@@ -8,5 +11,37 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Sse('stream')
+  stream() {
+    return new Observable((observer) => {
+      observer.next({ data: { msg: 'aaa' } });
+
+      setTimeout(() => {
+        observer.next({ data: { msg: 'bbb' } });
+      }, 2000);
+
+      setTimeout(() => {
+        observer.next({ data: { msg: 'ccc' } });
+      }, 5000);
+    });
+  }
+  @Sse('stream2')
+  stream2() {
+    const childProcess = exec('tail -f ./log');
+
+    return new Observable((observer) => {
+      childProcess.stdout!.on('data', (msg: string | Buffer) => {
+        observer.next({ data: { msg: msg.toString() } });
+      });
+    });
+  }
+  @Sse('stream3')
+  stream3() {
+    return new Observable((observer) => {
+      const json = readFileSync('./package.json').toJSON();
+      observer.next({ data: { msg: json } });
+    });
   }
 }
