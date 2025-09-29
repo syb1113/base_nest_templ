411fc1a237232f5b8cb24ec935b3ef41ec8d0f13
jiaoTailang <shiyib3719@gmail.com>
2025-09-29 14:33:30 +0800

    feat(app.controller): 添加文件下载接口

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header, Res, StreamableFile } from '@nestjs/common';
 import { AppService } from './app.service';
+import type { Response } from 'express';
+import * as fs from 'fs';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,31 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('download')
+  @Header('Content-Disposition', `attachment; filename="guang.json"`)
+  download(@Res() res: Response) {
+    const content = fs.readFileSync('package.json');
+
+    res.end(content);
+  }
+
+  @Get('download2')
+  @Header('Content-Disposition', `attachment; filename="guang.json"`)
+  download2(@Res() res: Response) {
+    const stream = fs.createReadStream('package.json');
+
+    stream.pipe(res);
+  }
+
+  @Get('download3')
+  download3() {
+    const stream = fs.createReadStream('package.json');
+
+    return new StreamableFile(stream, {
+      type: 'text/plain',
+      disposition: `attachment; filename="guang.json"`,
+    });
   }
 }
