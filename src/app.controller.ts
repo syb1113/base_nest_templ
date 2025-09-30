e42a4de09d0d64113915e7b9337888bc352124c6
jiaoTailang <shiyib3719@gmail.com>
2025-09-30 17:27:12 +0800

    feat(deps): 添加pptxgenjs和puppeteer依赖包
    新增pptxgenjs@^4.0.1用于生成PPTX文件功能
    新增puppeteer@^24.22.3用于浏览器自动化操作

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Sse } from '@nestjs/common';
 import { AppService } from './app.service';
-
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +7,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Sse('list')
+  universityList() {
+    // 添加返回类型声明
+    return this.appService.getUniversityData();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,101 @@
 import { Injectable } from '@nestjs/common';
+import puppeteer from 'puppeteer';
+import { Observable, Subscriber } from 'rxjs';
+import pptxgen from 'pptxgenjs';
+
+export interface UniversityData {
+  name: string;
+  img: string;
+  link: string;
+  desc: string;
+}
+let cache: UniversityData[];
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getUniversityData() {
+    if (cache) {
+      return cache;
+    }
+    async function getData(observer: Subscriber<Record<string, any>>) {
+      const browser = await puppeteer.launch({
+        headless: true,
+        defaultViewport: {
+          width: 0,
+          height: 0,
+        },
+      });
+
+      const page = await browser.newPage();
+
+      await page.goto('https://www.icourse163.org/university/view/all.htm');
+
+      await page.waitForSelector('.u-usitys');
+
+      const universityList = await page.$eval('.u-usitys', (el) => {
+        return [...el.querySelectorAll('.u-usity')].map((item) => {
+          return {
+            name: item.querySelector('img')!.alt,
+            img: item.querySelector('img')!.src,
+            link: item.getAttribute('href')!,
+            desc: '',
+          };
+        });
+      });
+
+      const ppt = new pptxgen();
+
+      for (const item of universityList) {
+        await page.goto(`https://www.icourse163.org${item.link}`);
+        await page.waitForSelector('.m-cnt');
+        const content = await page.$eval('.m-cnt', (el) => {
+          return el.textContent || '';
+        });
+        item.desc = content;
+        item.img = await page.$eval(
+          '.g-doc img',
+          (el) => el.getAttribute('src')!,
+        );
+
+        observer.next({ data: item });
+        const slide = ppt.addSlide();
+
+        slide.addText(item.name, {
+          x: '10%',
+          y: '10%',
+          color: '#ff0000',
+          fontSize: 30,
+          align: ppt.AlignH.center,
+        });
+
+        slide.addImage({
+          path: item.img,
+          x: '42%',
+          y: '25%',
+        });
+
+        slide.addText(item.desc, {
+          x: '10%',
+          y: '60%',
+          color: '#000000',
+          fontSize: 14,
+        });
+      }
+      await browser.close();
+
+      await ppt.writeFile({
+        fileName: '中国所有大学.pptx',
+      });
+
+      cache = universityList;
+    }
+
+    return new Observable((observer) => {
+      getData(observer);
+    });
   }
 }
