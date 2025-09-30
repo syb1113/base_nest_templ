98d40332ad430837cad79d8152a92fbc7db5a7a4
jiaoTailang <shiyib3719@gmail.com>
2025-09-30 16:00:28 +0800

    feat(qrcode): 添加二维码生成与扫描功能
    新增二维码登录相关接口和静态页面，支持二维码生成、状态检查、扫码确认及取消操作。
    - 引入 `qrcode` 库用于生成二维码
    - 实现 `/qrcode/generate` 接口生成二维码并初始化状态
    - 实现 `/qrcode/check` 接口查询二维码状态
    - 实现 `/qrcode/scan`、`/qrcode/confirm`、`/qrcode/cancel` 接口处理扫码流程
    - 添加静态资源目录，包含确认页面和首页展示二维码的页面
    - 配置 NestJS 使用 Express 并提供静态文件服务

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { randomUUID } from 'crypto';
+import * as qrcode from 'qrcode';
+interface QrCodeInfo {
+  status:
+    | 'noscan'
+    | 'scan-wait-confirm'
+    | 'scan-confirm'
+    | 'scan-cancel'
+    | 'expired';
+  userInfo?: {
+    userId: number;
+  };
+}
 
+const map = new Map<string, QrCodeInfo>();
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +22,63 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('qrcode/generate')
+  async generate() {
+    const uuid = randomUUID();
+    const dataUrl = await qrcode.toDataURL(
+      `http://192.168.62.129:3000/pages/confirm.html?id=${uuid}`,
+    );
+    map.set(`qrcode_${uuid}`, {
+      status: 'noscan',
+    });
+    return {
+      qrcode_id: uuid,
+      data_url: dataUrl,
+    };
+  }
+
+  @Get('qrcode/check')
+  check(@Query('id') id: string) {
+    const info = map.get(`qrcode_${id}`);
+    if (info && info.status === 'scan-confirm') {
+      return {
+        token: '获取用户token',
+        ...info,
+      };
+    }
+    return info;
+  }
+
+  //扫码后打开的登录页调用这个
+  @Get('qrcode/scan')
+  scan(@Query('id') id: string) {
+    const info = map.get(`qrcode_${id}`);
+    if (!info) {
+      throw new BadRequestException('二维码已过期');
+    }
+    info.status = 'scan-wait-confirm';
+    return 'success';
+  }
+  //扫码后用户确认调用
+  @Get('qrcode/confirm')
+  confirm(@Query('id') id: string) {
+    const info = map.get(`qrcode_${id}`);
+    if (!info) {
+      throw new BadRequestException('二维码已过期');
+    }
+    info.status = 'scan-confirm';
+    return 'success';
+  }
+  //扫码后用户取消调用
+  @Get('qrcode/cancel')
+  cancel(@Query('id') id: string) {
+    const info = map.get(`qrcode_${id}`);
+    if (!info) {
+      throw new BadRequestException('二维码已过期');
+    }
+    info.status = 'scan-cancel';
+    return 'success';
   }
 }
